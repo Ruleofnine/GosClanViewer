02230402316a92c0c9b7126402169f231dcefb94
Ruleofnine <ruleofnine@outlook.com>
2022-11-02 01:28:40 -0400

    added in argparse for ease of use

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ import sys
 import re
 from bs4 import BeautifulSoup
 from datetime import datetime,timedelta
+import argparse
+parser = argparse.ArgumentParser(prog="GoS Clan Viewer",description="displays info about your GoS clan!")
+parser.add_argument("filename",help="the filepath to the HTML of the clan members")
+parser.add_argument("-a","--active",action="store_true",help="shows all active players (default days is 30)")
+parser.add_argument("-i","--inactive",action="store_true",help="shows all inactive players (default days is 30)")
+parser.add_argument("-al","--all",action="store_true",help="Shows all players")
+parser.add_argument("-d","--days",help="number of days a player needs to be inactive before they count as inactive",type=int,default=30)
+args = parser.parse_args()
 clan_ranks = ['Clan Leader','Clan Co-Leader','Clan Lieutenant','Clan Sergeant','Clan General']
-overdue_days = timedelta(days=30) 
+overdue_days = timedelta(days=args.days) 
 def read_file(path):
     with open(path, "r+") as file1:
         return file1.read()
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        print("Please supply the path to the GoS clanpage HTML")
-        exit()
-    data = read_file(sys.argv[1])
+    data = read_file(args.filename)
     soup = BeautifulSoup(data, 'lxml')
     characters = soup.find_all(id=re.compile("myForm"))
     expired_characters = []
@@ -41,8 +46,17 @@ if __name__ == "__main__":
         else:
             active_characters.append(f"Rank: {chat_rank} Username: {username}, ID: {user_id} {date.strftime('%m/%d/%y')}  Activity: {player_activity} {status}")
             
+    
     print(f"Clan Members Online: {online_player_count} \nActive in last {overdue_days.days} days: {len(active_characters)}\nNot logged in for {overdue_days.days} days: {len(expired_characters)}")
-    for x in active_characters:
-        print(x)
+    if args.active:
+        for x in active_characters:
+            print(x)
+    if args.all:
+        players = active_characters + expired_characters
+        for x in players:
+            print(x)
+    if args.inactive:
+        for x in expired_characters:
+            print(x)
                 
 
